178c5565964b22432946bb44f21c543708f99d2e
Daniel González Cortés <47788332+gonzalezcortes@users.noreply.github.com>
2023-05-24 11:44:33 +0200

    Add project files.

---
+++ src/pre_processing.py
@@ -0,0 +1,37 @@
+#import pynini
+import numpy as np
+import pandas as pd
+
+
+class OpenData:
+    """
+    @staticmethod
+    def fst_read(file_path):
+        fst = pynini.Fst.read(file_path)
+        return fst
+    """
+    @staticmethod
+    def read_csv(file_path):
+        data = np.genfromtxt(file_path, delimiter=',', names=True, dtype=None)
+        return data
+
+    @staticmethod
+    def read_csv_columns(file_path):
+        df = pd.read_csv(file_path, nrows=0)
+        return df.columns
+
+    @staticmethod
+    def read_csv_header(file_path):
+        df = pd.read_csv(file_path, nrows=5)
+        return df
+
+    @staticmethod
+    def read_csv_chunks(file_path):
+        chunk_size = 10*6
+        chunks = []
+
+        for chunk in pd.read_csv(file_path, chunksize=chunk_size):
+            chunks.append(chunk)
+
+        df = pd.concat(chunks, axis=0)
+        return df
